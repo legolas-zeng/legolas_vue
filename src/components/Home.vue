6060f765c5c3805c62a72ae71c767cf06b27121d
legolas-zeng <814382690@qq.com>
2019-05-13 18:49:52 +0800

    vue添加home页面

---
+++ src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <el-row>
+      <el-button round>圆角按钮</el-button>
+      <el-button type="primary" round @click="go">dashboard</el-button>
+      <el-button type="success" round @click="back">返回index</el-button>
+      <el-button type="info" round>信息按钮</el-button>
+      <el-button type="warning" round>警告按钮</el-button>
+      <el-button type="danger" round>危险按钮</el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: '测试页'
+    }
+  },
+  methods:{
+  	go(){
+  		this.$router.push('/dashboard')
+  	},
+    back(){
+  	  this.$router.push('/')
+    }
+  }
+}
+</script>
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import MenuView from '@/components/leftSlide'
+// import DashBoard from '@/pages/dashboard'
 
 Vue.use(Router)
 
@@ -10,6 +13,14 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    }, {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+    }, {
+      path: '/dashboard',
+      name: 'DashBoard',
+      component: MenuView,
     }
   ]
 })
